70d92546c6c22157b469550e258dcc734a5a9e38
LTF <ltf@users.noreply.github.com>
2016-06-01 10:24:30 +0800

    testSmali: test variable set value

--- adrlab/testSmali/app/src/main/java/com/f/testsmali/MainActivity.java
+++ adrlab/testSmali/app/src/main/java/com/f/testsmali/MainActivity.java
@@ -10,11 +10,20 @@ import android.os.RemoteException;
 public class MainActivity extends Activity {
     private static final String ID = "111";
 
+    protected String xxxId;
+    protected String xxxId2;
+    protected String xxxId3;
 
     public String getId() {
         return "222";
     }
     public String getConstId() {
         return ID;
+    }
+    public void initXXXId() {
+        xxxId = ID;
+        xxxId2 = "123123";
+        String tmp = ID;
+        xxxId3 = tmp;
     }
 }
