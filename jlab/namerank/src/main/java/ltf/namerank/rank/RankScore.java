9e8625e8fa2eb72b917d8774c7b197ef1c3c10de
LTF <ltf@users.noreply.github.com>
2016-07-07 22:53:18 +0800

    namerank: tmp add rankScore

--- jlab/namerank/src/main/java/ltf/namerank/rank/RankScore.java
+++ jlab/namerank/src/main/java/ltf/namerank/rank/RankScore.java
@@ -1,12 +1,41 @@
 package ltf.namerank.rank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ltf
  * @since 16/7/7, 下午4:48
  */
 public class RankScore {
-    public RankScore add(RankScore summand) {
 
+    private double defaultValue = 0;
+
+    private Map<String, Double> values = new HashMap<>();
+
+    /**
+     * addition
+     */
+    public RankScore add(RankScore summand) {
+        this.defaultValue += summand.defaultValue;
         return this;
     }
+
+    /**
+     * addition
+     */
+    public RankScore add(double summand) {
+        this.defaultValue += summand;
+        return this;
+    }
+
+    /**
+     * multiplication
+     */
+    public RankScore mul(double multiplicand) {
+        this.defaultValue *= multiplicand;
+        return this;
+    }
+
+
 }
