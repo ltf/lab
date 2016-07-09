272d01ec35ff50417f8d7a2e0bb1a512daa03db0
LTF <ltf@users.noreply.github.com>
2016-07-10 01:11:22 +0800

    namerank: remove dict cache

--- jlab/namerank/src/main/java/ltf/namerank/rank/dictrank/support/dict/MdxtDict.java
+++ jlab/namerank/src/main/java/ltf/namerank/rank/dictrank/support/dict/MdxtDict.java
@@ -1,39 +1,27 @@
 package ltf.namerank.rank.dictrank.support.dict;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.JSONSerializer;
-import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.sun.istack.internal.NotNull;
 import ltf.namerank.rank.RankItem;
 import ltf.namerank.rank.Ranker;
 import ltf.namerank.rank.dictrank.support.PinyinMap;
-import ltf.namerank.service.EvenManager;
-import ltf.namerank.service.TeardownListener;
 import ltf.namerank.utils.LinesInFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.alibaba.fastjson.serializer.SerializerFeature.PrettyFormat;
-import static com.alibaba.fastjson.serializer.SerializerFeature.WriteClassName;
 import static ltf.namerank.rank.RankItemHelper.addInfo;
 import static ltf.namerank.rank.RankItemHelper.flushResult;
-import static ltf.namerank.utils.FileUtils.file2Str;
-import static ltf.namerank.utils.FileUtils.str2File;
 
 /**
  * @author ltf
  * @since 16/6/21, 下午4:40
  */
-abstract public class MdxtDict implements Ranker, TeardownListener {
+abstract public class MdxtDict implements Ranker {
 
     private final Logger logger = LoggerFactory.getLogger(MdxtDict.class);
     private static final String ITEM_END_LINE = "</>";
@@ -44,13 +32,9 @@ abstract public class MdxtDict implements Ranker, TeardownListener {
 
     protected Map<String, List<MdxtItem>> itemsMap;
 
-    public MdxtDict() {
-        EvenManager.add(this);
-    }
 
     protected void initItems() {
         long start = System.currentTimeMillis();
-        if (loadCache()) return;
         if (itemsMap == null) {
             itemsMap = new HashMap<>();
             try {
@@ -124,43 +108,6 @@ abstract public class MdxtDict implements Ranker, TeardownListener {
         return itemsMap;
     }
 
-    @Override
-    public void onTeardown() {
-        if (itemsMap != null && itemsMap.size() > 0 && !new File(getCacheFilename()).exists())
-            saveCache();
-    }
-
-    private String getCacheFilename() {
-        return getFileName() + ".cache";
-    }
-
-    private void saveCache() {
-        try {
-            FileWriter fileWriter = new FileWriter(getCacheFilename());
-            SerializeWriter out = new SerializeWriter(fileWriter, WriteClassName, PrettyFormat);
-
-            try {
-                JSONSerializer serializer = new JSONSerializer(out);
-                serializer.write(itemsMap);
-            } finally {
-                out.close();
-            }
-        } catch (Exception e) {
-            logger.error("save cache failed", e);
-        }
-    }
-
-    private boolean loadCache() {
-        try {
-            if (new File(getCacheFilename()).exists()) {
-                itemsMap = (Map<String, List<MdxtItem>>) JSON.parseObject(file2Str(getCacheFilename()), HashMap.class);
-                return true;
-            }
-        } catch (Exception e) {
-            logger.error("load cache failed", e);
-        }
-        return false;
-    }
 }
 
 
