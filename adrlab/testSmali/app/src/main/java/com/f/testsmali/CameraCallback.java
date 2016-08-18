da8ade57f5e23749f7667cf33833dc32306443c7
LTF <ltf@users.noreply.github.com>
2016-08-18 12:04:18 +0800

    testSmali: add test smali code

---
+++ adrlab/testSmali/app/src/main/java/com/f/testsmali/CameraCallback.java
@@ -0,0 +1,12 @@
+package com.f.testsmali;
+
+
+import android.hardware.Camera;
+
+/**
+ * @author ltf
+ * @since 16/8/18, 上午9:01
+ */
+public class CameraCallback{
+
+}
--- adrlab/testSmali/app/src/main/java/com/f/testsmali/MainActivity.java
+++ adrlab/testSmali/app/src/main/java/com/f/testsmali/MainActivity.java
@@ -1,7 +1,10 @@
 package com.f.testsmali;
 
 import android.app.Activity;
-import android.os.RemoteException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author ltf
@@ -17,13 +20,34 @@ public class MainActivity extends Activity {
     public String getId() {
         return "222";
     }
+
     public String getConstId() {
         return ID;
     }
+
     public void initXXXId() {
         xxxId = ID;
         xxxId2 = "123123";
         String tmp = ID;
         xxxId3 = tmp;
     }
+
+    public void testLoadPic() {
+        byte[] x = file2Array();
+    }
+
+
+    public byte[] file2Array() {
+        try {
+            FileInputStream in = new FileInputStream("/sdcard/x.jpg");
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024 * 12];
+            int len = 0;
+            while ((len = in.read(buf)) > 0) out.write(buf, 0, len);
+            return out.toByteArray();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
 }
