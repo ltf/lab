d9704e232f0fd5c5277dc0a938f9a5166c97f090
LTF <ltf@users.noreply.github.com>
2016-11-14 16:43:27 +0800

    uilab: add test draw order codes

---
+++ adrlab/ui/uilab/app/src/main/java/l/tf/uilab/DrawOrderActivity.java
@@ -0,0 +1,16 @@
+package l.tf.uilab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author ltf
+ * @since 16/11/14, 下午4:35
+ */
+public class DrawOrderActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.draw_order_layout);
+    }
+}
