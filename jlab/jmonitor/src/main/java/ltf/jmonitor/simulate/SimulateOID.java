149c0f7cde5ce38403851c92c309d10745164d74
LTF <ltf@users.noreply.github.com>
2017-04-08 00:55:10 +0800

    jmonitor: udapte submit wait time

--- jlab/jmonitor/src/main/java/ltf/jmonitor/simulate/SimulateOID.java
+++ jlab/jmonitor/src/main/java/ltf/jmonitor/simulate/SimulateOID.java
@@ -19,7 +19,7 @@ public class SimulateOID extends SimulateOASIS {
             submitReq();
             continueReq();
             try {
-                Thread.sleep(1000 * 60*2);
+                Thread.sleep(1000 * 30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
