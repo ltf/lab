057ac91739f567fee3585a8bd67803f746664728
LTF <ltf@users.noreply.github.com>
2017-04-08 17:30:34 +0800

    pmonitor: auto commit and update

--- php/pmonitor/s.php
+++ php/pmonitor/s.php
@@ -5,7 +5,11 @@ $urls = array(
 	'http://www.saskatchewan.ca/residents/moving-to-saskatchewan/immigrating-to-saskatchewan/saskatchewan-immigrant-nominee-program/maximum-number-of-sinp-applications',
 	'http://www.saskatchewan.ca'
 );
+$len = count($urls);
+if (is_int($_GET['i'])) $i = $_GET['i'];
+else $i = rand();
+$url=$urls[$i % $len];
 $html = file_get_contents($url); 
 $hash = hash('crc32b', $html); 
-echo $_GET['i']; 
+echo $url; 
 ?>
