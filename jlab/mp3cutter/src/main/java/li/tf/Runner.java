2798729c547387d8e768d7fe19e63369b0beac45
LTF <ltf@users.noreply.github.com>
2017-05-04 15:50:06 +0800

    add mp3cutter project

---
+++ jlab/mp3cutter/src/main/java/li/tf/Runner.java
@@ -0,0 +1,10 @@
+package li.tf;
+
+/**
+ * @author ltf
+ * @since 17/5/4, 下午3:31
+ */
+public class Runner {
+
+
+}
---
+++ jlab/mp3cutter/src/main/java/li/tf/mp3cutter/TagID3v2.java
@@ -0,0 +1,10 @@
+package li.tf.mp3cutter;
+
+/**
+ * @author ltf
+ * @since 17/5/4, 下午3:49
+ */
+public class TagID3v2 {
+
+
+}
