7b0146cadc8d17c92cead06bfdfeb3ca2ca10228
LTF <ltf@users.noreply.github.com>
2017-05-05 16:41:32 +0800

    add parse codes

--- jlab/mp3cutter/src/main/java/li/tf/mp3cutter/Mp3Cutter.java
+++ jlab/mp3cutter/src/main/java/li/tf/mp3cutter/Mp3Cutter.java
@@ -1,6 +1,12 @@
 package li.tf.mp3cutter;
 
-import java.io.*;
+import li.tf.mp3cutter.section.Mp3Frame;
+import li.tf.mp3cutter.section.Section;
+import li.tf.mp3cutter.section.SectionDetector;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * @author ltf
@@ -14,16 +20,29 @@ public class Mp3Cutter {
         this.inFile = new RandomAccessFile(inFile, "r");
     }
 
-    public static void cut(String inFile, String outFile, int start, int end) {
-
+    public static void cut(String inFile, String outFile, int start, int end) throws IOException {
+        new Mp3Cutter(inFile).cutTo(start, end, outFile);
     }
 
     public void cutTo(int start, int end, String outFile) throws IOException {
         byte[] head = new byte[10];
-        inFile.read(head);
+        int size;
+        size = inFile.read(head);
+        SectionDetector detector = new SectionDetector();
+        Section sec = null;
 
+        while (size > 0) {
+            if ((sec = detector.detect(head)) != null) {
+                System.out.println(sec.getClass().getSimpleName());
+                if (sec instanceof Mp3Frame) {
+                    inFile.skipBytes(sec.getLength() - head.length);
 
+                } else {
+                    inFile.skipBytes(sec.getLength() - head.length);
+                }
+                size = inFile.read(head);
+            }
 
-
+        }
     }
 }
--- jlab/mp3cutter/src/main/java/li/tf/mp3cutter/section/SectionDetector.java
+++ jlab/mp3cutter/src/main/java/li/tf/mp3cutter/section/SectionDetector.java
@@ -5,6 +5,20 @@ package li.tf.mp3cutter.section;
  * @since 17/5/4, 下午5:07
  */
 public class SectionDetector {
+    private Section[] sectionTypes = {
+            new TagID3v2Section(),
+            new TagID3v2Footer(),
+            new TagID3v1Section(),
+            new Mp3Frame()
+    };
 
 
+    public Section detect(byte[] head) {
+        for (Section sec : sectionTypes) {
+            if (sec.parseHead(head)) {
+                return sec;
+            }
+        }
+        return null;
+    }
 }
