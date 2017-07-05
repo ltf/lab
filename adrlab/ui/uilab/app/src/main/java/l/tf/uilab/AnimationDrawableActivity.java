4265e8b541118c5565db432fb8ff8ed98217c449
LTF <ltf@users.noreply.github.com>
2017-07-05 10:20:26 +0800

    test relative layout draworder and layout

---
+++ adrlab/ui/uilab/app/src/main/java/l/tf/uilab/AnimationDrawableActivity.java
@@ -0,0 +1,13 @@
+package l.tf.uilab;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class AnimationDrawableActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_drawable);
+    }
+}
---
+++ adrlab/ui/uilab/app/src/main/java/l/tf/uilab/RelativeLayoutDrawOrderActivity.java
@@ -0,0 +1,13 @@
+package l.tf.uilab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeLayoutDrawOrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_draw_order);
+    }
+}
