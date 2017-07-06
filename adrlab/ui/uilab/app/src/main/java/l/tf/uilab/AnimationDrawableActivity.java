bf4f0f30657f7ea3a06296079443ec8113ba1a4d
LTF <ltf@users.noreply.github.com>
2017-07-06 17:02:30 +0800

    add AnimationDrawable test code

--- adrlab/ui/uilab/app/src/main/java/l/tf/uilab/AnimationDrawableActivity.java
+++ adrlab/ui/uilab/app/src/main/java/l/tf/uilab/AnimationDrawableActivity.java
@@ -1,13 +1,23 @@
 package l.tf.uilab;
 
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class AnimationDrawableActivity extends ActionBarActivity {
+public class AnimationDrawableActivity extends Activity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_drawable);
+
+
+        mImageView = (ImageView) findViewById(R.id.list_animation_image_view);
+        AnimationDrawable animaDrawable = (AnimationDrawable) mImageView.getDrawable();
+        animaDrawable.start();
     }
 }
