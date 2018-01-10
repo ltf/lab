86f2c45d798caf989170be4163b545026d1322dd
LTF <ltf@users.noreply.github.com>
2018-01-10 15:17:49 +0800

    adrserver: add adr server project

---
+++ jlab/adrserver/src/main/java/f/Main.java
@@ -0,0 +1,26 @@
+package f;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println(getClass().getResource("/").getFile());
+
+        java.net.URL loc = getClass().getResource("/sample.fxml");
+        Parent root = FXMLLoader.load(loc);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ jlab/adrserver/src/main/java/f/Runner.java
@@ -0,0 +1,13 @@
+package f;
+
+/**
+ * @author ltf
+ * @since 2018/1/10, 上午10:39
+ */
+public class Runner {
+
+    public boolean someLibraryMethod() {
+
+        return true;
+    }
+}
