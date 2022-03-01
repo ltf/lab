44307766170f4ce7687c95b34102f48207db26ac
f <f@tshb.w10>
2022-03-02 01:23:53 +0800

    namerank: prepare good wuge candidates

--- jlab/namerank/src/main/java/ltf/namerank/dataprepare/PipinameDataCollect.java
+++ jlab/namerank/src/main/java/ltf/namerank/dataprepare/PipinameDataCollect.java
@@ -4,10 +4,7 @@ import com.alibaba.fastjson.TypeReference;
 import ltf.namerank.entity.DictItem_Bm8;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static ltf.namerank.utils.FileUtils.*;
@@ -18,6 +15,26 @@ import static ltf.namerank.utils.FileUtils.*;
  */
 public class PipinameDataCollect {
     private static final String PIPI_WUGE_STROKES_SHORT = "pipiname_wuge_char2stroke";
+
+    private static final int[] BEST_NUMS = {1, 3, 5, 7, 11, 13, 15, 16, 18, 21, 24, 25, 31, 32, 33, 35, 37, 39, 41, 45, 47, 48, 52, 57, 61, 63, 65, 67, 68, 81};
+    //private static final int[] BEST_NUMS = {1, 3, 5, 7, 8, 11, 13, 15, 16, 18, 21, 23, 24, 25, 31, 32, 33, 35, 37, 39, 41, 45, 47, 48, 52, 57, 61, 63, 65, 67, 68, 81};
+
+    private static final int[] GOOD_NUMS = {1, 3, 5, 6, 7, 8, 11, 13, 15, 16, 18, 21, 23, 24, 25, 29, 31, 32, 33, 35, 37, 39, 41, 45, 47, 48, 52, 57, 61, 63, 65, 67, 68, 81};
+
+    private static final String[] WUXING = {
+            "水",
+            "木", "木",
+            "火", "火",
+            "土", "土",
+            "金", "金",
+            "水"};
+
+    private static final String[] SANCAI_GOOD = {
+            "木木木", "木木火", "木木土", "木火木", "木火土", "木水木", "木水金", "木水水", "火木木", "火木火",
+            "火木土", "火火木", "火火土", "火土火", "火土土", "火土金", "土火木", "土火火", "土火土", "土土火",
+            "土土土", "土土金", "土金土", "土金金", "土金水", "金土火", "金土土", "金土金", "金金土", "金水木",
+            "金水金", "水木木", "水木火", "水木土", "水木水", "水金土", "水金水", "水水木", "水水金"};
+
 
     private void saveWugeStrokeFromPipiname() throws IOException {
         ArrayList<String> lines = new ArrayList<>();
@@ -77,12 +94,112 @@ public class PipinameDataCollect {
         toJsData(ppS2c, "selected_wuge_stroke2char");
 
 
-
         //toLinesData(diff, "diff");
+    }
+
+    private void cleanSelectedCharacters() throws IOException {
+        HashMap<String, Integer> ppC2s = fromJsData("selected_wuge_char2stroke", new TypeReference<HashMap<String, Integer>>() {
+        });
+
+        ArrayList<String> cs = new ArrayList<>();
+        fromLinesData("namecs", cs);
+        HashSet<String> csset = new HashSet<>();
+        cs.forEach(line -> {
+            for (char c : line.toCharArray()) {
+                csset.add(String.valueOf(c));
+            }
+        });
+        cs.clear();
+        ppC2s.forEach((k, v) -> {
+            if (!csset.contains(k))
+                cs.add(k);
+        });
+
+        cs.forEach(ppC2s::remove);
+
+        toJsData(ppC2s, "selected_wuge_char2stroke");
+
+        HashMap<Integer, List<String>> ppS2c = new HashMap<>();
+        ppC2s.forEach((k, v) -> {
+            ppS2c.putIfAbsent(v, new ArrayList<>());
+            ppS2c.get(v).add(k);
+        });
+        toJsData(ppS2c, "selected_wuge_stroke2char");
+    }
+
+    private void calculateBestStrokes() throws IOException {
+        StrokePicker picker = new StrokePicker();
+        HashMap<Integer, List<String>> ppS2c = fromJsData("selected_wuge_stroke2char", new TypeReference<HashMap<Integer, List<String>>>() {
+
+        });
+
+        ArrayList<String> candidates = new ArrayList<>(1000000);
+
+        for (int ming1 : ppS2c.keySet()) {
+            for (int ming2 : ppS2c.keySet()) {
+                if (picker.isGood(ming1, ming2)) {
+                    int count = fullSetNames(ppS2c.get(ming1), ppS2c.get(ming2), candidates);
+
+                    System.out.println(
+                            Integer.toString(ming1) + " - " + Integer.toString(ming2)
+                                    + " : " + Integer.toString(count));
+                }
+            }
+        }
+
+        toLinesData(candidates, "candidates");
+    }
+
+    private int fullSetNames(List<String> ming1, List<String> ming2, List<String> candidates) {
+        for (String m1 : ming1) {
+            for (String m2 : ming2) {
+                candidates.add(m1 + m2);
+            }
+        }
+        return ming1.size() * ming2.size();
+    }
+
+    private static class StrokePicker {
+        static final int xing = 7;
+        // 天格
+        static final int tian = xing + 1;
+
+        static final boolean[] GOOD_NUM = new boolean[300];
+        static final HashSet<String> GOOD_SANCAI = new HashSet<>();
+
+        static {
+            Arrays.stream(BEST_NUMS).forEach(i -> GOOD_NUM[i] = true);
+            GOOD_SANCAI.addAll(Arrays.asList(SANCAI_GOOD));
+        }
+
+        public boolean isGood(int ming1, int ming2) {
+            // 人格
+            int ren = xing + ming1;
+            // 地格
+            int di = ming1 + ming2;
+            // 总格
+            int zong = xing + ming1 + ming2;
+            // 外格
+            int wai = zong - ren + 1;
+
+            if (GOOD_NUM[ren] && GOOD_NUM[di] && GOOD_NUM[zong] && GOOD_NUM[wai]) {
+                // 三才配置
+                StringBuilder sc = new StringBuilder(wuxing(tian));
+                sc.append(wuxing(ren)).append(wuxing(di));
+                return GOOD_SANCAI.contains(sc.toString());
+            }
+            return false;
+        }
+    }
+
+    private static String wuxing(int strokes) {
+        return WUXING[strokes % 10];
     }
 
     public static void main(String[] args) throws IOException {
         //new PipinameDataCollect().saveWugeStrokeFromPipiname();
-        new PipinameDataCollect().checkData();
+        //new PipinameDataCollect().checkData();
+        new PipinameDataCollect().calculateBestStrokes();
+        //new PipinameDataCollect().cleanSelectedCharacters();
     }
 }
